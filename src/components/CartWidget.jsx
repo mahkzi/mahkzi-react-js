91e31e10254b1c6c1211ba1adab8e64a3f30410d
Maximo Castro <maximocastrobraga@gmail.com>
2024-12-03 10:22:31 -0300

    arreglamos el problema de la img, listo para entrega

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,7 @@
 function CartWidget(){
     return(
 <div className="div-carrito">
-    <img src="./assets/carrito.png" alt="img-carrito" />
+    <img src="../src/assets/carrito.png" alt="img-carrito" />
     <p style={{padding:"10px"}}>3</p>
 </div>
     )
